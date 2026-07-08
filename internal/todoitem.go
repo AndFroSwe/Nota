8ca3e08976938b182a15a6273eea53d3ca45f29e
andfroswe <andfro@protonmail.com>
2026-07-08 20:41:08 +0200

    Add date parsing

--- internal/todoitem.go
+++ internal/todoitem.go
@@ -38,18 +38,52 @@ func getStatus(s *string) (TodoStatus, error) {
 	}
 }
 
+func getDate(s *string) (time.Time, error) {
+	if strings.TrimSpace(*s) == "" {
+		return time.Time{}, errors.New("empty string")
+	}
+
+	start := strings.Index(*s, "<")
+	if start == -1 {
+		return time.Time{}, errors.New("No <")
+	}
+
+	end := strings.Index((*s)[start+1:], ">")
+	if end == -1 {
+		return time.Time{}, errors.New("No >")
+	}
+
+	extracted := (*s)[start+1 : start+1+end]
+	d, err := time.Parse("060102", extracted)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return d, nil
+}
+
 func ParseTodoLine(s *string) (Todo, error) {
 	t := Todo{
 		raw:      *s,
 		status:   NOT_TODO,
-		deadline: time.Now(),
+		deadline: time.Time{},
 	}
 
 	status, err := getStatus(s)
+	if status == NOT_TODO {
+		return t, nil
+	}
+
 	if err != nil {
 		return t, err
 	}
 	t.status = status
+
+	d, err := getDate(s)
+	if err != nil {
+		return t, nil
+	}
+	t.deadline = d
 
 	return t, nil
 }
--- internal/todoitem_test.go
+++ internal/todoitem_test.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"testing"
+	"time"
 )
 
 func TestRaw(t *testing.T) {
@@ -58,5 +59,36 @@ func TestParseStatus(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestParseDate(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    time.Time
+		wantErr bool
+	}{
+		{"Invalid month", "- [ ] <303102> Some text", time.Time{}, false},
+		{"Invalid day", "- [ ] <300232> Some text", time.Time{}, false},
+		{"Invalid date", "- [ ] <not_a_date> Some text", time.Time{}, false},
+		{"Empty date", "- [ ] <> Some text", time.Time{}, false},
+		{"Not a date", "- [ ] <not_a_date> Some text", time.Time{}, false},
+		{"Not a todo", "<260102> Not a todo", time.Time{}, false},
+		{"Open, no close", "Not a < date", time.Time{}, false},
+		{"simple date", "- [ ] <260707> Some text", time.Date(2026, 07, 07, 0, 0, 0, 0, time.UTC), false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T){
+			got, err := ParseTodoLine(&tt.input)
+
+			if err != nil && !tt.wantErr {
+				t.Errorf("error parsing '%s': %v", tt.input, err)
+			}
+
+			if got.deadline != tt.want {
+				t.Errorf("incorrect result parsing '%s'. Expected %v, got %v", tt.input, tt.want, got.deadline)
+			}
+		})
+	}
 }
