d919590f0ea5fbacb5c86ba210bd2cac203fa60c
andfroswe <andfro@protonmail.com>
2026-07-24 13:36:32 +0200

    fix(style): Fix typos and non-idiomatic go usages

--- internal/cli/app.go
+++ internal/cli/app.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/andfroswe/nota/internal/todoitem"
+	todoitem "github.com/andfroswe/nota/internal/todoitem"
 
 	"github.com/jedib0t/go-pretty/v6/table"
 	"golang.org/x/term"
@@ -19,14 +19,14 @@ import (
 
 // Table parameters
 const (
-	wTotMin   = 120                                                  // Min total width of terminal to present meaningful data
-	wDate     = 12                                                   // Width of date column
-	wResp     = 12                                                   // Width of responsible column
-	wStatus   = 1                                                    // Width of status column
-	wTagsMin  = 18                                                   // Min width for tags column
-	wCols     = 5                                                    // Number of columns
-	wOverhead = (wCols + 1) + (wCols-1)*2                            // Rendering overhead. Separators + padding
-	wMsgMin   = wTotMin - wDate - wResp - wStatus - wTagsMin - wCols // Calculate the min width to use for the message column
+	minTotalWidth       = 120                                                                                               // Min total width of terminal to present meaningful data
+	minDateWidth        = 12                                                                                                // Width of date column
+	minResponisbleWidth = 12                                                                                                // Width of responsible column
+	minStatusWidth      = 1                                                                                                 // Width of status column
+	minTagsWidth        = 18                                                                                                // Min width for tags column
+	numberOfCols        = 5                                                                                                 // Number of columns
+	renderOverhead      = (numberOfCols + 1) + (numberOfCols-1)*2                                                           // Rendering overhead. Separators + padding
+	minMessageWidth     = minTotalWidth - minDateWidth - minResponisbleWidth - minStatusWidth - minTagsWidth - numberOfCols // Calculate the min width to use for the message column
 )
 
 // programOpts are the CLI program options
@@ -37,6 +37,16 @@ type programOpts struct {
 	useNerdfont  bool   // True if nerd fonts should be used
 	sortBy       string // Column to sort by
 	sortAsc      bool   // Sort ascending if true, descending otherwise
+}
+
+// tableSize is the size informations for displaying the table
+// Column width in glyphs
+type tableSize struct {
+	wMaxStatus      int
+	wMaxMsg         int
+	wMaxResponsible int
+	wMaxTags        int
+	wMaxDate        int
 }
 
 // Run is the main routine for using the CLI
@@ -77,10 +87,8 @@ func Run() int {
 	case "markdown":
 		t.RenderMarkdown()
 	case "color":
-		{
-			t.SetStyle(table.StyleColoredDark)
-			t.Render()
-		}
+		t.SetStyle(table.StyleColoredDark)
+		t.Render()
 	}
 
 	return 0
@@ -91,15 +99,6 @@ func createTable(opts programOpts, tableSize *tableSize) table.Writer {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(table.Row{"T", "Activity", "Tags", "Responsible", "Deadline"})
-
-	// Helper to print string slices
-	sliceTransformer := func(val any) string {
-		if s, ok := val.([]string); ok {
-			return strings.Join(s, ",")
-		}
-
-		return fmt.Sprintf("%v", val) // Fallback
-	}
 
 	// Configure table style
 	t.SetColumnConfigs([]table.ColumnConfig{
@@ -125,7 +124,7 @@ func createTable(opts programOpts, tableSize *tableSize) table.Writer {
 		{
 			Name:        "Deadline",
 			WidthMax:    tableSize.wMaxDate,
-			Transformer: getDateTransformer(),
+			Transformer: dateTransformer,
 		},
 	})
 
@@ -143,8 +142,8 @@ func createTable(opts programOpts, tableSize *tableSize) table.Writer {
 		sortName = "Deadline"
 	case "tags":
 		sortName = "Tags"
-	case "responsiblep":
-		sortName = "Resonsible"
+	case "responsible":
+		sortName = "Responsible"
 	default:
 		log.Panicf("invalid sort key")
 	}
@@ -155,51 +154,56 @@ func createTable(opts programOpts, tableSize *tableSize) table.Writer {
 	return t
 }
 
-// getDateTransformer returns a transformer function to display dates in table
-func getDateTransformer() func(val any) string {
-	dateTransformer := func(val any) string {
-		if d, ok := val.(*time.Time); ok {
-			if d == nil {
-				return ""
-			}
-
-			if internal.IsTBD(*d) {
-				return "TBD"
-			}
-
-			return d.Format("2006-01-02")
+// dateTransformer transforms a date to desired format
+var dateTransformer = func(val any) string {
+	if d, ok := val.(*time.Time); ok {
+		if d == nil {
+			return ""
 		}
 
-		return fmt.Sprintf("%v", val)
+		if todoitem.IsTBD(*d) {
+			return "TBD"
+		}
+
+		return d.Format("2006-01-02")
 	}
 
-	return dateTransformer
+	return fmt.Sprintf("%v", val)
+}
+
+// sliceTransformer formats string slices for printing
+var sliceTransformer = func(val any) string {
+	if s, ok := val.([]string); ok {
+		return strings.Join(s, ",")
+	}
+
+	return fmt.Sprintf("%v", val) // Fallback
 }
 
 // getTodoTransformer takes programOpts and returns a transformer for displaying todo statuses
 func getTodoTransformer(opts programOpts) func(val any) string {
 	todoTransformer := func(val any) string {
-		if t, ok := val.(internal.TodoStatus); ok {
+		if t, ok := val.(todoitem.TodoStatus); ok {
 			if opts.useNerdfont {
 				switch t {
-				case internal.StatusNotTodo:
+				case todoitem.StatusNotTodo:
 					return ""
-				case internal.StatusOpen:
+				case todoitem.StatusOpen:
 					return " "
-				case internal.StatusDone:
+				case todoitem.StatusDone:
 					return "󰄬"
-				case internal.StatusCanceled:
+				case todoitem.StatusCanceled:
 					return "󰜺"
 				}
 			} else {
 				switch t {
-				case internal.StatusNotTodo:
+				case todoitem.StatusNotTodo:
 					return "E"
-				case internal.StatusOpen:
+				case todoitem.StatusOpen:
 					return " "
-				case internal.StatusDone:
+				case todoitem.StatusDone:
 					return "x"
-				case internal.StatusCanceled:
+				case todoitem.StatusCanceled:
 					return "-"
 				}
 			}
@@ -210,16 +214,6 @@ func getTodoTransformer(opts programOpts) func(val any) string {
 	return todoTransformer
 }
 
-// tableSize is the size informations for displaying the table
-// Column width in glyphs
-type tableSize struct {
-	wMaxStatus      int
-	wMaxMsg         int
-	wMaxResponsible int
-	wMaxTags        int
-	wMaxDate        int
-}
-
 // calcTableSize calculates the size of the columns in the table based on parameters and the size of the terminal
 func calcTableSize() (*tableSize, error) {
 	terminalWidth, _, err := term.GetSize(0) // Get the current terminal size
@@ -228,24 +222,24 @@ func calcTableSize() (*tableSize, error) {
 	}
 
 	// Need enough space to give meaningful output
-	if terminalWidth < wTotMin {
-		return nil, fmt.Errorf("terminal too narrow for output (%d < %d)", terminalWidth, wTotMin)
+	if terminalWidth < minTotalWidth {
+		return nil, fmt.Errorf("terminal too narrow for output (%d < %d)", terminalWidth, minTotalWidth)
 	}
 
 	tz := tableSize{
-		wMaxDate:        wDate,
-		wMaxResponsible: wResp,
-		wMaxStatus:      wStatus,
+		wMaxDate:        minDateWidth,
+		wMaxResponsible: minResponisbleWidth,
+		wMaxStatus:      minStatusWidth,
 	}
-	const ratioMsg = 0.8                                                                    // The relative size of the dynamic columns to give to msg
-	dynCols := terminalWidth - tz.wMaxDate - tz.wMaxResponsible - tz.wMaxStatus - wOverhead // Available columns for dynamics sizing
+	const ratioMsg = 0.8                                                                         // The relative size of the dynamic columns to give to msg
+	dynCols := terminalWidth - tz.wMaxDate - tz.wMaxResponsible - tz.wMaxStatus - renderOverhead // Available columns for dynamics sizing
 	tz.wMaxMsg = max(
 		int(ratioMsg*float64(dynCols)), // Dynamic size
-		wMsgMin,
+		minMessageWidth,
 	)
 	tz.wMaxTags = max(
 		dynCols-tz.wMaxMsg, // Left from msg size
-		wTagsMin,           // Min allowed size
+		minTagsWidth,       // Min allowed size
 	)
 
 	return &tz, nil
@@ -266,7 +260,7 @@ func parseFlags() (programOpts, error) {
 	flag.BoolVar(&opts.useNerdfont, "u", true, "Use nerdfont symbols. May need to be false on older terminals")
 	flag.StringVar(&opts.sortBy, "s", sortColumns[0], fmt.Sprintf("Sort by [%v]", sortColumns))
 	flag.BoolVar(&opts.sortAsc, "a", true, "Sort ascending [true/false]")
-	flag.Parse() // Parse input flags
+	flag.Parse()
 
 	// Validate input date
 	if !slices.Contains(outputFormats, opts.outputFormat) {
@@ -283,8 +277,8 @@ func parseFlags() (programOpts, error) {
 }
 
 // getTodos finds the files to check and calls parsing on them
-func getTodos(dir string, recurse bool) ([]internal.Todo, error) {
-	var todos []internal.Todo
+func getTodos(dir string, recurse bool) ([]todoitem.Todo, error) {
+	var todos []todoitem.Todo
 	err := filepath.WalkDir(dir, func(path string, d os.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -313,8 +307,8 @@ func getTodos(dir string, recurse bool) ([]internal.Todo, error) {
 	return todos, nil
 }
 
-// parseFile takes a path and pointer to todo slice and adds todos from file at path to the slice or returns an error
-func parseFile(path string, todos []internal.Todo) ([]internal.Todo, error) {
+// parseFile takes a path and a todo slice and adds todos from file. It returs a todoitem.Todo slice and and error
+func parseFile(path string, todos []todoitem.Todo) ([]todoitem.Todo, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return todos, fmt.Errorf("opening %s: %w", path, err)
@@ -326,12 +320,12 @@ func parseFile(path string, todos []internal.Todo) ([]internal.Todo, error) {
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		todo, err := internal.ParseTodoLine(line)
+		todo, err := todoitem.ParseTodoLine(line)
 		if err != nil {
 			continue
 		}
 
-		if todo.Status == internal.StatusNotTodo {
+		if todo.Status == todoitem.StatusNotTodo {
 			continue
 		}
 
